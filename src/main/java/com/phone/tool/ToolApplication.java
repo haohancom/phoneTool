0594caf83cdcae945812fee611513ca063328ff0
Aaron Hao (hanhao) <hanhao@cisco.com>
2021-10-01 20:45:53 +0800

    clint can send msg to server

--- src/main/java/com/phone/tool/ToolApplication.java
+++ src/main/java/com/phone/tool/ToolApplication.java
@@ -1,13 +1,38 @@
 package com.phone.tool;
 
+import com.phone.tool.netty.EchoServer;
+import io.netty.channel.ChannelFuture;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToolApplication {
+public class ToolApplication implements CommandLineRunner {
+
+	@Value("${netty.port}")
+	private int port;
+
+	@Value("${netty.url}")
+	private String url;
+
+	@Autowired
+	private EchoServer echoServer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToolApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		ChannelFuture future = echoServer.start(url, port);
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				echoServer.destroy();
+			}
+		});
+		future.channel().closeFuture().syncUninterruptibly();
+	}
 }
---
+++ src/main/java/com/phone/tool/netty/EchoServer.java
@@ -0,0 +1,64 @@
+package com.phone.tool.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.net.InetSocketAddress;
+
+@Slf4j
+@Component
+public class EchoServer {
+
+    private final EventLoopGroup bossGroup = new NioEventLoopGroup();
+    private final EventLoopGroup workerGroup = new NioEventLoopGroup();
+    private Channel channel;
+
+    public ChannelFuture start(String hostname, int port) throws Exception {
+
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        ChannelFuture f = null;
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(hostname,port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(serverHandler);
+                        }
+                    });
+
+            f = b.bind().sync();
+            channel = f.channel();
+            log.info("======EchoServer startup=========");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (f != null && f.isSuccess()) {
+                log.info("Netty server listening " + hostname + " on port " + port + " and ready for connections...");
+            } else {
+                log.error("Netty server start up Error!");
+            }
+        }
+        return f;
+    }
+
+    public void destroy() {
+        log.info("Shutdown Netty Server...");
+        if (channel != null) {
+            channel.close();
+        }
+        workerGroup.shutdownGracefully();
+        bossGroup.shutdownGracefully();
+        log.info("Shutdown Netty Server Success!");
+    }
+}
